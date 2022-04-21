fa1d7fdda64e05b03a56ba815ca9f63d51127b32
Gsx115 <gsx115@gmail.com>
2022-04-20 21:00:31 -0700

    Grid creation now dynamic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,13 @@
 const grid = document.querySelector('.container');
 
 function genGrid(){
-    for(let i = 1; i <= 256; i++){
-        let gridItem = document.createElement('div');
-        gridItem.classList.add('item');
-        grid.appendChild(gridItem);
+    for(let y = 1; y <= 16; y++){
+        for(let x = 1; x <= 16; x++){
+            let gridItem = document.createElement('div');
+            gridItem.classList.add('item');
+            if(x==1) gridItem.classList.add('row-start');
+            grid.appendChild(gridItem);
+        }
     }
 }
 
