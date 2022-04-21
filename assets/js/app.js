e3be56f7a6df751a225174144aee5f0a611cdee1
Gsx115 <gsx115@gmail.com>
2022-04-21 11:14:22 -0700

    Add Clear Board Button and prompt user for input

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,35 @@
 const grid = document.querySelector('.container');
+const clear = document.querySelector('#clear-board');
 
-function genGrid(){
-    for(let y = 1; y <= 16; y++){
-        for(let x = 1; x <= 16; x++){
+function newBoard(){
+    let rows = window.prompt('Please enter number of rows less than 100');
+    let cols = window.prompt('Please enter number of columns less than 100');
+    if (isNaN(rows) || isNaN(cols) || rows === null || cols === null || rows > 100 || cols > 100){
+        alert('Please enter numbers only.  Try again.')
+        newBoard();
+    }
+    genGrid(parseInt(rows),parseInt(cols));
+}
+
+function genGrid(x,y){
+    console.log(`Rows: ${x}, Cols: ${y}`);
+    for(let a = 1; a <= y; a++){
+        for(let b = 1; b <= x; b++){
             let gridItem = document.createElement('div');
             gridItem.classList.add('item');
-            if(x==1) gridItem.classList.add('row-start');
+            if(b==1) gridItem.classList.add('row-start');
             grid.appendChild(gridItem);
         }
     }
 }
 
-grid.addEventListener('mouseover',(e)=>e.target.classList.add('hover'));
+grid.addEventListener('mouseover',(e)=>{
+    if (e.target.classList.contains('item')) e.target.classList.add('hover');
+});
+
+clear.addEventListener('click',(e)=>location.reload());
+
 
     
 
-genGrid();
+newBoard();
