2e14a8e1c4eee0d4827a182bf1da07c9e10f3ff7
Gsx115 <gsx115@gmail.com>
2022-04-20 20:21:45 -0700

    Changed layout a bit. Added Hover style

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 const grid = document.querySelector('.container');
 
 function genGrid(){
-    for(let i = 1; i <= 16; i++){
+    for(let i = 1; i <= 256; i++){
         let gridItem = document.createElement('div');
         gridItem.classList.add('item');
         grid.appendChild(gridItem);
